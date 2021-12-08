8e36adfa03ec2716146c8d483ca6f206f0b256d9
Richard Evans <rickecon@gmail.com>
2021-12-08 01:14:07 -0700

    Updated OGplots.py

--- OGplots.py
+++ OGplots.py
@@ -51,9 +51,9 @@ df2['DebtGDP_ref_G033'] = (df2['D_ref_G033'] / df2['Y_ref_G033']) * 100
 df2['DebtGDP_ref_T340'] = (df2['D_ref_T340'] / df2['Y_ref_T340']) * 100
 
 
-def gen_tseries2(frcst_var_list, legend_label_list, df, color_list,
-                start_year='min', end_year='max', note_text_list=[],
-                fig_title_str='', fig_path=''):
+def gen_tseries_dy(var_list, legend_label_list, df, color_list, marker_list,
+                   start_year='min', end_year='max', note_text_list=[],
+                   fig_title_str='', fig_path=''):
     """
     This function creates a plot of multiple time series of CBO forecasts of
     U.S. publicly held national debt.
@@ -74,13 +74,12 @@ def gen_tseries2(frcst_var_list, legend_label_list, df, color_list,
     min_yvar = 100
     max_yvar = 0
     cds_list = []
-    for k, yvar in enumerate(frcst_var_list):
+    for k, yvar in enumerate(var_list):
         min_yvar = np.minimum(min_yvar, df_main[yvar].min())
         max_yvar = np.maximum(max_yvar, df_main[yvar].max())
         frcst_df = df_main[['year', yvar]].dropna()
-        frcst_df['frcst_label'] = legend_label_list[k]
         frcst_df.rename(columns={yvar: 'debt_gdp'}, inplace=True)
-        frcst_df = frcst_df[['year', 'debt_gdp', 'frcst_label']]
+        frcst_df = frcst_df[['year', 'debt_gdp']]
         cds_list.append(ColumnDataSource(frcst_df))
 
     # Output to HTML file
@@ -110,17 +109,14 @@ def gen_tseries2(frcst_var_list, legend_label_list, df, color_list,
     fig.yaxis.ticker = SingleIntervalTicker(interval=10, num_minor_ticks=2)
     fig.ygrid.ticker = SingleIntervalTicker(interval=10)
 
-    # Create lines and markers for two time series
-    fig.line(x='year', y='debt_gdp', source=cds_list[0], color=color_list[0],
-             line_width=3, alpha=0.7, muted_alpha=0.15)
-    fig.triangle(x='year', y='debt_gdp', source=cds_list[0], size=9,
-                 line_width=1, line_color='black', fill_color=color_list[0],
-                 alpha=0.7, muted_alpha=0.2, legend_label=legend_label_list[0])
-    fig.line(x='year', y='debt_gdp', source=cds_list[1], color=color_list[1],
-             line_width=3, alpha=0.7, muted_alpha=0.15)
-    fig.circle(x='year', y='debt_gdp', source=cds_list[1], size=8,
-                 line_width=1, line_color='black', fill_color=color_list[1],
-                 alpha=0.7, muted_alpha=0.2, legend_label=legend_label_list[1])
+    # Create lines and markers for time series
+    for k, yvar in enumerate(var_list):
+        fig.line(x='year', y='debt_gdp', source=cds_list[k], color=color_list[k],
+                line_width=3, alpha=0.7)
+        fig.scatter(x='year', y='debt_gdp', source=cds_list[k], size=8,
+                    line_width=1, line_color='black', fill_color=color_list[k],
+                    marker=marker_list[k], line_alpha=0.7, fill_alpha=0.7,
+                    legend_label=legend_label_list[k])
 
     # Add vertical dashed line at 2050
     fig.segment(x0=2050, y0=90, x1=2050, y1=215, color='black',
@@ -128,8 +124,7 @@ def gen_tseries2(frcst_var_list, legend_label_list, df, color_list,
 
     # Add information on hover
     tooltips = [('Year', '@year'),
-                ('Debt/GDP','@debt_gdp'),
-                ('Forecast date', '@frcst_label')]
+                ('Debt/GDP','@debt_gdp{0.0}' + '%')]
     fig.add_tools(HoverTool(tooltips=tooltips, toggleable=False))
 
     # Add legend
@@ -138,9 +133,6 @@ def gen_tseries2(frcst_var_list, legend_label_list, df, color_list,
     fig.legend.border_line_color = 'black'
     fig.legend.border_line_alpha = 1
     fig.legend.label_text_font_size = '4mm'
-
-    # Set legend muting click policy
-    fig.legend.click_policy = 'mute'
 
     # Add notes below image
     for note_text in note_text_list:
@@ -151,9 +143,9 @@ def gen_tseries2(frcst_var_list, legend_label_list, df, color_list,
     return fig
 
 
-def gen_tseries3(frcst_var_list, legend_label_list, df, color_list,
-                 start_year='min', end_year='max', note_text_list=[],
-                 fig_title_str='', fig_path=''):
+def gen_tseries_macro(var_list, legend_label_list, df, color_list, marker_list,
+                      start_year='min', end_year='max', note_text_list=[],
+                      fig_title_str='', fig_path=''):
     """
     This function creates a plot of multiple time series of CBO forecasts of
     U.S. publicly held national debt.
@@ -174,13 +166,12 @@ def gen_tseries3(frcst_var_list, legend_label_list, df, color_list,
     min_yvar = 100
     max_yvar = 0
     cds_list = []
-    for k, yvar in enumerate(frcst_var_list):
+    for k, yvar in enumerate(var_list):
         min_yvar = np.minimum(min_yvar, df_main[yvar].min())
         max_yvar = np.maximum(max_yvar, df_main[yvar].max())
         frcst_df = df_main[['year', yvar]].dropna()
-        frcst_df['frcst_label'] = legend_label_list[k]
-        frcst_df.rename(columns={yvar: 'debt_gdp'}, inplace=True)
-        frcst_df = frcst_df[['year', 'debt_gdp', 'frcst_label']]
+        frcst_df.rename(columns={yvar: 'var_pct_chg'}, inplace=True)
+        frcst_df = frcst_df[['year', 'var_pct_chg']]
         cds_list.append(ColumnDataSource(frcst_df))
 
     y_buffer_pct = 0.1
@@ -192,13 +183,12 @@ def gen_tseries3(frcst_var_list, legend_label_list, df, color_list,
     fig_path = fig_path
     output_file(fig_path, title=fig_title)
 
-
     fig = figure(title=fig_title,
                  plot_height=600,
                  plot_width=1100,
                  x_axis_label='Year',
                  x_range=(min_year - 1, max_year + 1),
-                 y_axis_label='Percent of Gross Domestic Product',
+                 y_axis_label='Percent change',
                  y_range=(min_yvar - y_buffer_amt, max_yvar + y_buffer_amt),
                  toolbar_location=None)
 
@@ -212,34 +202,25 @@ def gen_tseries3(frcst_var_list, legend_label_list, df, color_list,
     # Modify tick intervals for X-axis and Y-axis
     fig.xaxis.ticker = SingleIntervalTicker(interval=5, num_minor_ticks=5)
     fig.xgrid.ticker = SingleIntervalTicker(interval=5)
-    fig.yaxis.ticker = SingleIntervalTicker(interval=10, num_minor_ticks=2)
-    fig.ygrid.ticker = SingleIntervalTicker(interval=10)
+    fig.yaxis.ticker = SingleIntervalTicker(interval=2, num_minor_ticks=2)
+    fig.ygrid.ticker = SingleIntervalTicker(interval=2)
 
-    # Create lines and markers for two time series
-    fig.line(x='year', y='debt_gdp', source=cds_list[0], color=color_list[0],
-             line_width=3, alpha=0.7, muted_alpha=0.15)
-    fig.circle(x='year', y='debt_gdp', source=cds_list[0], size=8,
-                 line_width=1, line_color='black', fill_color=color_list[0],
-                 alpha=0.7, muted_alpha=0.2, legend_label=legend_label_list[0])
-    fig.line(x='year', y='debt_gdp', source=cds_list[1], color=color_list[1],
-             line_width=3, alpha=0.7, muted_alpha=0.15)
-    fig.triangle(x='year', y='debt_gdp', source=cds_list[1], size=9,
-                 line_width=1, line_color='black', fill_color=color_list[1],
-                 alpha=0.7, muted_alpha=0.2, legend_label=legend_label_list[1])
-    fig.line(x='year', y='debt_gdp', source=cds_list[2], color=color_list[2],
-             line_width=3, alpha=0.7, muted_alpha=0.15)
-    fig.square(x='year', y='debt_gdp', source=cds_list[2], size=8,
-                 line_width=1, line_color='black', fill_color=color_list[2],
-                 alpha=0.7, muted_alpha=0.2, legend_label=legend_label_list[2])
+    # Create lines and markers for time series
+    for k, yvar in enumerate(var_list):
+        fig.line(x='year', y='var_pct_chg', source=cds_list[k],
+                 color=color_list[k], line_width=3, alpha=0.7)
+        fig.scatter(x='year', y='var_pct_chg', source=cds_list[k], size=8,
+                    line_width=1, line_color='black', fill_color=color_list[k],
+                    marker=marker_list[k], line_alpha=0.7, fill_alpha=0.7,
+                    legend_label=legend_label_list[k])
 
     # Add vertical dashed line at 2050
-    fig.segment(x0=2050, y0=90, x1=2050, y1=215, color='black',
+    fig.segment(x0=2050, y0=-2, x1=2050, y1=14, color='black',
                 line_dash='6 4', line_width=2)
 
     # Add information on hover
     tooltips = [('Year', '@year'),
-                ('Debt/GDP','@debt_gdp'),
-                ('Forecast date', '@frcst_label')]
+                ('Percent change', '@var_pct_chg{0.0}' + '%')]
     fig.add_tools(HoverTool(tooltips=tooltips, toggleable=False))
 
     # Add legend
@@ -248,9 +229,6 @@ def gen_tseries3(frcst_var_list, legend_label_list, df, color_list,
     fig.legend.border_line_color = 'black'
     fig.legend.border_line_alpha = 1
     fig.legend.label_text_font_size = '4mm'
-
-    # Set legend muting click policy
-    fig.legend.click_policy = 'mute'
 
     # Add notes below image
     for note_text in note_text_list:
@@ -268,6 +246,7 @@ if __name__ == "__main__":
     # Create publicly held debt forecasts figure
     frcst_var_list1 = ['mar_2021', 'ogusa']
     color_list1 = ['red', 'blue']
+    marker_list1 = ['triangle', 'circle']
     legend_label_list1 = [
         'CBO forecast (Mar. 2021)', 'OG-USA baseline forecast']
     note_text_list1 = \
@@ -283,22 +262,27 @@ if __name__ == "__main__":
     fig_path1 = os.path.join(images_dir,
                              'tseries_pubdebt_gdp_cbo_ogusa_frcsts.html')
     pubdebt_gdp_frcsts_cbo_ogusa_tseries = \
-        gen_tseries2(frcst_var_list1, legend_label_list1, df1, color_list1,
-                     start_year=2021, end_year=2051,
-                     note_text_list=note_text_list1,
-                     fig_title_str=fig_title1, fig_path=fig_path1)
+        gen_tseries_dy(frcst_var_list1, legend_label_list1, df1, color_list1,
+                       marker_list1, start_year=2021, end_year=2051,
+                       note_text_list=note_text_list1,
+                       fig_title_str=fig_title1, fig_path=fig_path1)
     show(pubdebt_gdp_frcsts_cbo_ogusa_tseries)
 
     # Create figure of baseline and reform debt-to-GDP time paths
     frcst_var_list2 = ['DebtGDP_base', 'DebtGDP_ref_G033', 'DebtGDP_ref_T340']
     color_list2 = ['blue', 'green', '#C584DB']
+    marker_list2 = ['circle', 'triangle', 'square']
     legend_label_list2 = [
         'Current law baseline', 'Gov\'t discretionary spending cut',
-        'Per. income and corporate tax increase']
+        'Pers. income and corporate tax increase']
     note_text_list2 = \
         [
             ('Source: OG-USA baseline and reform forecasts from simulations ' +
-             'in Appendix D.'),
+             'in Appendix D. One reform is to reduce government ' +
+             'discretionary spending by 1.7 per-'),
+            ('   centage points. The other reform is to increase personal ' +
+             'income marginal tax rates and the corporate income tax rate ' +
+             'by 34 percent.'),
         ]
 
     fig_title2 = ('Debt-to-GDP in Two Reforms Forecasts versus Baseline ' +
@@ -306,8 +290,45 @@ if __name__ == "__main__":
     fig_path2 = os.path.join(images_dir,
                              'tseries_pubdebt_gdp_G033_T340.html')
     pubdebt_gdp_G033_T340_tseries = \
-        gen_tseries3(frcst_var_list2, legend_label_list2, df2, color_list2,
-                     start_year=2021, end_year=2055,
-                     note_text_list=note_text_list2,
-                     fig_title_str=fig_title2, fig_path=fig_path2)
+        gen_tseries_dy(frcst_var_list2, legend_label_list2, df2, color_list2,
+                       marker_list2, start_year=2021, end_year=2055,
+                       note_text_list=note_text_list2,
+                       fig_title_str=fig_title2, fig_path=fig_path2)
     show(pubdebt_gdp_G033_T340_tseries)
+
+    # Plot macro aggregates percent changes from OG-USA gov't spending cut
+    df2['Y_pctchg_G033'] = ((df2['Y_ref_G033'] - df2['Y_base']) /
+                            df2['Y_base']) * 100
+    df2['C_pctchg_G033'] = ((df2['C_ref_G033'] - df2['C_base']) /
+                            df2['C_base']) * 100
+    df2['K_pctchg_G033'] = ((df2['K_ref_G033'] - df2['K_base']) /
+                            df2['K_base']) * 100
+    df2['L_pctchg_G033'] = ((df2['L_ref_G033'] - df2['L_base']) /
+                            df2['L_base']) * 100
+    frcst_var_list3 = ['Y_pctchg_G033', 'C_pctchg_G033', 'K_pctchg_G033',
+                       'L_pctchg_G033']
+    color_list3 = ['blue', 'orange', 'green', 'red']
+    marker_list3 = ['circle', 'triangle', 'square', 'square_pin']
+    legend_label_list3 = [
+        'GDP', 'Aggregate consumption', 'Aggregate capital stock',
+        'Aggregate labor']
+    note_text_list3 = \
+        [
+            ('Source: Macroeconomic effect of cutting government ' +
+             'discretionary spending as a percent of GDP by 1.7 percentage ' +
+             'points. Percent changes in macro-'),
+            ('   economic variables come from OG-USA baseline and reform ' +
+             'forecasts from simulations in Appendix D.'),
+        ]
+
+    fig_title3 = ('Macroeconomic Aggregates, Percent Change from Baseline ' +
+                  'from Cut to Government Discretionary Spending, 2021 to ' +
+                  '2055')
+    fig_path3 = os.path.join(images_dir,
+                             'MacroAgg_PctChange_G033.html')
+    MacroAgg_PctChange_G033 = \
+        gen_tseries_macro(frcst_var_list3, legend_label_list3, df2,
+                          color_list3, marker_list3, start_year=2021,
+                          end_year=2055, note_text_list=note_text_list3,
+                          fig_title_str=fig_title3, fig_path=fig_path3)
+    show(MacroAgg_PctChange_G033)
